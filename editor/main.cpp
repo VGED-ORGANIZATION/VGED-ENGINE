a8075f40667e33eec1c7470036d7d6510df88464
lukasino1214 <lukasino1214@gmail.com>
2022-08-19 10:24:24 +0200

    Project Hierarchy

---
+++ editor/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Vulkan Game Engine Dev!\n";
+    return 0;
+}
---
+++ engine/core/window.cpp
@@ -0,0 +1,28 @@
+#include "window.h"
+
+#include "debug.h"
+
+namespace vged {
+    namespace core {
+        Window::Window(int width, int height, const char *name) 
+            : m_width(width),
+            m_height(height),
+            m_name(name) {
+            if (!glfwInit()) {
+                THROW("GLFW failed to initialize!");
+            }
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);  // todo: change this to true
+
+            m_window = glfwCreateWindow(m_width, m_height, m_name.c_str(), NULL, NULL);
+            if (!m_window) {
+                THROW("Failed to create glfw window!");
+            }
+        }
+
+        Window::~Window() {
+            glfwDestroyWindow(m_window);
+            glfwTerminate();
+        }
+    } // namespace core
+} // namespace vged

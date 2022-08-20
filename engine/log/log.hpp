f55c810eb8c8850a55e13e25d2b0867325a68f04
sivansh11 <sivanshgupta2002@gmail.com>
2022-08-21 00:18:00 +0530

    added log macros
    VGED_ENGINE_LOGLEVEL is for engine logs
    VGED_LOGLEVEL is for client logs

--- engine/log/log.hpp
+++ engine/log/log.hpp
@@ -22,9 +22,21 @@ namespace VGED
         }
 
     private: 
-
         static std::shared_ptr<spdlog::logger> m_EngineLogger;
         static std::shared_ptr<spdlog::logger> m_AppLogger;
-
     };
 }
+
+#ifndef DISTRIBUTION  // disables engine logging (currently its never disabled)  
+#define VGED_ENGINE_TRACE(...) ::VGED::Log::getLogger()->trace(__VA_ARGS__)
+#define VGED_ENGINE_INFO(...) ::VGED::Log::getLogger()->infor(__VA_ARGS__)
+#define VGED_ENGINE_WARN(...) ::VGED::Log::getLogger()->warn(__VA_ARGS__)
+#define VGED_ENGINE_ERROR(...) ::VGED::Log::getLogger()->error(__VA_ARGS__)
+#define VGED_ENGINE_FATAL(...) ::VGED::Log::getLogger()->fatal(__VA_ARGS__)
+#endif
+
+#define VGED_TRACE(...) ::VGED::Log::getAppLogger()->trace(__VA_ARGS__)
+#define VGED_INFO(...) ::VGED::Log::getAppLogger()->infor(__VA_ARGS__)
+#define VGED_WARN(...) ::VGED::Log::getAppLogger()->warn(__VA_ARGS__)
+#define VGED_ERROR(...) ::VGED::Log::getAppLogger()->error(__VA_ARGS__)
+#define VGED_FATAL(...) ::VGED::Log::getAppLogger()->fatal(__VA_ARGS__)

808343a2cb641dc798f769a428ce24ebc6fa0851
Kael Johnston <wkaelj@gmail.com>
2022-08-23 21:11:13 -0600

    Implement loader

---
+++ engine/core/loader.cpp
@@ -0,0 +1,80 @@
+#include "loader.hpp"
+
+#include <dlfcn.h>
+
+namespace VGED {
+namespace Engine {
+
+namespace HotLoader {
+
+inline int convert_loading_mode(LoadingModes mode)
+{
+    switch (mode) {
+    case LOAD_LAZY: return RTLD_LAZY;
+    case LOAD_NOW: return RTLD_NOW;
+    default: return RTLD_LAZY;
+    }
+}
+
+SharedLibrary::SharedLibrary(std::string path, LoadingModes mode)
+{
+
+    dlptr_ = dlopen(path.c_str(), convert_loading_mode(mode));
+    if (dlptr_ == NULL)
+    {
+        std::string err = "Failed to load shared object: ";
+        err = err.append(path);
+        throw std::runtime_error(err); // why no printf formatting :(
+    }
+}
+
+SharedLibrary::~SharedLibrary()
+{
+    dlclose(dlptr_);
+}
+
+template <typename T>
+inline T SharedLibrary::get_function(std::string name)
+{
+    return (T) dlsym(dlptr_, name.c_str());
+}
+
+Executable::Executable(
+    std::string path,
+    std::string entryPoint,
+    LoadingModes mode) : entryPoint_(entryPoint)
+{
+    lib_ = new SharedLibrary(path, mode);
+    return;
+}
+
+Executable::~Executable()
+{
+    delete lib_;
+}
+
+int Executable::run(std::vector<std::string> args)
+{
+    int (*main)(int, char**) = lib_->get_function<int(*)(int, char**)>(entryPoint_);
+
+    // convert args
+    char *cargs[args.size()];
+    for (size_t i = 0; i < args.size(); ++i) {
+        cargs[i] = (char*) args[i].c_str(); // FIXME idk probably shouldn't cast
+    }
+    return main(args.size(), cargs);
+}
+
+int Executable::change_library(std::string path, LoadingModes loadMode)
+{
+    delete lib_;
+    lib_ = new SharedLibrary(path, loadMode);
+
+    if (lib_ == NULL) return EXIT_FAILURE;
+    return EXIT_SUCCESS;
+}
+
+} // HotLoader
+
+} // Engine
+} // VGED
--- engine/core/loader.hpp
+++ engine/core/loader.hpp
@@ -1,5 +1,7 @@
 #include "vged.hpp"
 
+#include "stdexcept"
+
 namespace VGED {
 namespace Engine {
 
@@ -14,7 +16,78 @@ enum LoadingModes {
     LOAD_NOW
 };
 
-void loadLibrary(std::string path, LoadingModes mode);
+class SharedLibrary {
+
+    public:
+    SharedLibrary(std::string path, LoadingModes mode = LOAD_LAZY);
+    ~SharedLibrary();
+
+    /**
+     * @brief Get the function object
+     *
+     * @tparam T the type of the function
+     * @param name the name of the function
+     * @return T a pointer to the function, or NULL
+     */
+    template <typename T>
+    inline T get_function(std::string name);
+
+    const void *get_lib(void) { return dlptr_; }
+
+    private:
+    void *dlptr_;
+
+};
+
+/**
+ * @brief Store and execute loaded code
+ *
+ */
+class Executable {
+
+    public:
+
+    /**
+     * @brief Construct a new Executable object
+     *
+     * @param path the path to the shared object
+     * @param entryPoint the main function name
+     * @param mode the method of loading the file
+     */
+    Executable(
+        std::string path,
+        std::string entryPoint = "main",
+        LoadingModes mode = LOAD_LAZY);
+
+    ~Executable(void);
+
+    // TODO deep copy
+    Executable operator=(Executable&)=delete;
+    Executable(Executable&)=delete;
+
+    int run(std::vector<std::string> args);
+
+    /**
+     * @brief Change the libary loaded in the executable
+     *
+     * @param path the path to the new shared object
+     * @return int
+     */
+    int change_library(std::string path, LoadingModes loadMode = LOAD_LAZY);
+
+    /**
+     * @brief Get the libary memory
+     *
+     * @return void*
+     */
+    inline const SharedLibrary *get_libary(void) { return lib_; }
+
+    private:
+    SharedLibrary *lib_ = NULL;
+    std::string entryPoint_;
+    LoadingModes loadMode_;
+
+};
 
 } // HotLoader
 

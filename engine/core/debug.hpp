4483a04d1d7a0fa0543356d8436b501ef26d94a0
Kael Johnston <wkaelj@gmail.com>
2022-08-24 20:07:29 -0600

    Updated debug.hpp formatting
    Updated the debug.hpp formatting so that error messages work as links in vscode, and also added function names so that it is easier to read the messeges.

--- engine/core/debug.hpp
+++ engine/core/debug.hpp
@@ -5,12 +5,12 @@
 
 #include "log.hpp"
 
-#ifndef NDEBUG 
+#ifndef NDEBUG
 #define ASSERT(condition, message) \
     do { \
         if (! (condition)) { \
             std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
-                      << " (" << __LINE__ << "): " << message << std::endl; \
+                      << ":" << __LINE__ << "): " << message << std::endl; \
             std::terminate(); \
         } \
     } while (false)
@@ -20,4 +20,6 @@
 
 #endif
 
-#define THROW(message) std::cerr << "Error in file " << __FILE__ << "(" << __LINE__ << "): " << message << std::endl; std::terminate()
+#define THROW(message) std::cerr << \
+    "Error in function " << __func__ << " (" << \
+    __FILE__ << ":" << __LINE__, "): " << message << std::endl; std::terminate()

7ba91319f1737112bf58f7164c1005bf2f5e56c1
Kael Johnston <wkaelj@gmail.com>
2022-08-24 20:21:00 -0600

    Improvments.
    Fixed to use some best practices, and updated so that not all asserts need an error messege. Some asserts should be self explanatory, and do not need explanations. I believe this will encourage the use of descriptive variable and function names.

--- engine/core/debug.hpp
+++ engine/core/debug.hpp
@@ -6,20 +6,37 @@
 #include "log.hpp"
 
 #ifndef NDEBUG
-#define ASSERT(condition, message) \
+#define ASSERT_MSG(condition, message) \
     do { \
         if (! (condition)) { \
-            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
-                      << ":" << __LINE__ << "): " << message << std::endl; \
-            std::terminate(); \
+            std::cerr << "Asserton (" << #condition << \
+            ") failed in " << __func__ << " (" << \
+            __FILE__ << ":" << __LINE__, "): " << \
+            message << std::endl; std::terminate(); \
         } \
     } while (false)
 
 #else
-#define ASSERT(condition, message);
-
+#define ASSERT_MSG(condition, message) ((void)0)
 #endif
 
-#define THROW(message) std::cerr << \
-    "Error in function " << __func__ << " (" << \
-    __FILE__ << ":" << __LINE__, "): " << message << std::endl; std::terminate()
+#ifdef NDEBUG
+#define ASSERT(condition) \
+    do { \
+        if (!(condition)) { \
+            std::cerr << "Asserton (" << #condition << \
+            ") failed in " << __func__ << " (" << \
+            __FILE__ << ":" << __LINE__, ")" << std::endl; \
+            std::terminate(); \
+        } \
+    } while(0)
+#else
+#define ASSERT(condition) ((void)0)
+#endif
+
+#define THROW(message) \
+    do { \
+        std::cerr << "Error in function " << __func__ << " (" << \
+        __FILE__ << ":" << __LINE__ << "): " << message << std::endl; \
+        std::terminate(); \
+    } while(0)

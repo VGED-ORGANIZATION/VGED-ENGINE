208a7662d29ce6b42cfa7fac905bd9f6ad750aec
Kael Johnston <wkaelj@gmail.com>
2022-08-29 19:43:39 -0600

    Revert "Revert "Auto stash before checking out "wkaelj/dev"""
    This reverts commit c8bcaed628a5892c8b3c40211d98b467b3fdea86.

--- engine/core/loader.hpp
+++ engine/core/loader.hpp
@@ -1,9 +1,12 @@
 #include "vged.hpp"
 
 #include "stdexcept"
+#include "types.hpp"
 
 namespace VGED {
 namespace Engine {
+
+inline namespace Core {
 
 /**
  * @brief Load .so or .dll files, and access functions within them
@@ -11,32 +14,27 @@ namespace Engine {
  */
 namespace HotLoader {
 
-enum LoadingModes {
-    LOAD_LAZY = 0,
-    LOAD_NOW
-};
+enum LoadingModes { LOAD_LAZY = 0, LOAD_NOW };
 
 class SharedLibrary {
 
-    public:
-    SharedLibrary(std::string path, LoadingModes mode = LOAD_LAZY);
-    ~SharedLibrary();
+public:
+	SharedLibrary(std::string path, LoadingModes mode = LOAD_LAZY);
+	~SharedLibrary();
 
-    /**
-     * @brief Get the function object
-     *
-     * @tparam T the type of the function
-     * @param name the name of the function
-     * @return T a pointer to the function, or NULL
-     */
-    template <typename T>
-    inline Result<T> get_function(std::string name);
+	/**
+	 * @brief Get the function object
+	 *
+	 * @tparam T the type of the function
+	 * @param name the name of the function
+	 * @return T a pointer to the function, or NULL
+	 */
+	template <typename T> Result<T> get_function(std::string name);
 
-    const void *get_lib(void) { return dlptr_; }
+	const void *get_lib(void) { return dlptr_; }
 
-    private:
-    void *dlptr_;
-
+private:
+	void *dlptr_;
 };
 
 /**
@@ -45,51 +43,47 @@ class SharedLibrary {
  */
 class Executable {
 
-    public:
+public:
+	/**
+	 * @brief Construct a new Executable object
+	 *
+	 * @param path the path to the shared object
+	 * @param entryPoint the main function name
+	 * @param mode the method of loading the file
+	 */
+	Executable(std::string path, std::string entry_point = "main",
+			   LoadingModes mode = LOAD_LAZY);
 
-    /**
-     * @brief Construct a new Executable object
-     *
-     * @param path the path to the shared object
-     * @param entryPoint the main function name
-     * @param mode the method of loading the file
-     */
-    Executable(
-        std::string path,
-        std::string entryPoint = "main",
-        LoadingModes mode = LOAD_LAZY);
+	~Executable(void);
 
-    ~Executable(void);
+	// TODO deep copy
+	Executable operator=(Executable &) = delete;
+	Executable(Executable &) = delete;
 
-    // TODO deep copy
-    Executable operator=(Executable&)=delete;
-    Executable(Executable&)=delete;
+	int run(std::vector<std::string> args);
 
-    int run(std::vector<std::string> args);
+	/**
+	 * @brief Change the libary loaded in the executable
+	 *
+	 * @param path the path to the new shared object
+	 * @return int
+	 */
+	int change_library(std::string path, LoadingModes load_mode = LOAD_LAZY);
 
-    /**
-     * @brief Change the libary loaded in the executable
-     *
-     * @param path the path to the new shared object
-     * @return int
-     */
-    int change_library(std::string path, LoadingModes loadMode = LOAD_LAZY);
+	/**
+	 * @brief Get the libary memory
+	 *
+	 * @return void*
+	 */
+	inline const SharedLibrary *get_libary(void) { return lib_; }
 
-    /**
-     * @brief Get the libary memory
-     *
-     * @return void*
-     */
-    inline const SharedLibrary *get_libary(void) { return lib_; }
-
-    private:
-    SharedLibrary *lib_ = NULL;
-    std::string entryPoint_;
-    LoadingModes loadMode_;
-
+private:
+	SharedLibrary *lib_ = NULL;
+	std::string entry_point_;
+	LoadingModes load_mode_;
 };
 
 } // HotLoader
-
+} // Core
 } // Engine
 } // VGED
